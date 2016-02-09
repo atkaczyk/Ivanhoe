c701786aec0460f3875ec139b08cbe44af42cf9c
Sophia <sophia.c.b@hotmail.com>
2016-02-09 11:12:46 -0500

    Adding base files

---
+++ src/test/testSuite/TestSuite.java
@@ -0,0 +1,18 @@
+package testSuite;
+
+//import gameRulesTest.TestGame;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+//	TestGame.class,
+//	TestPlayer.class,
+//	TestRulesEngine.class,
+//	TestClient.class
+})
+
+public class TestSuite {
+
+}
