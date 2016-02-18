cc35ee11e851e13989509168279a47aed7d5af28
Sophia <sophia.c.b@hotmail.com>
2016-02-18 12:54:13 -0500

    Initialize players in game

--- src/main/logic/Game.java
+++ src/main/logic/Game.java
@@ -2,9 +2,15 @@ package logic;
 
 public class Game {
 	private int numOfPlayers;
+	private Player[] players;
 	
 	public void setNumPlayers(int numOfPlayers) {
 		this.numOfPlayers = numOfPlayers;
+		players = new Player[numOfPlayers];
+		
+		for (int i = 0; i < numOfPlayers; i++) {
+			players[i] = new Player();
+		}
 	}
 
 	public int getNumPlayers() {
