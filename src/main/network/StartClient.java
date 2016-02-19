7aa01561e0d21e01aace978b3a4e3a5973ff08e8
Victoria Gray <Victoria Gray>
2016-02-19 16:29:44 -0500

    new server starting

---
+++ src/main/network/StartClient.java
@@ -0,0 +1,12 @@
+package network;
+
+//import network.Client;
+import utils.Config;
+
+public class StartClient {
+
+	public static void main (String[] argv){
+		//new Client(Config.DEFAULT_HOST, Config.DEFAULT_PORT)
+	}
+	
+}
