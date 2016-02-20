a5eb3cf8f973a2ecc79ea989f23a8009d7ead8c4
Sophia <sophia.c.b@hotmail.com>
2016-02-20 13:50:28 -0500

    Pick tournament colour, only supporter in hand test pass

--- src/test/logicTest/TestGame.java
+++ src/test/logicTest/TestGame.java
@@ -7,6 +7,7 @@ import java.util.ArrayDeque;
 import logic.Card;
 import logic.ColourCard;
 import logic.Game;
+import logic.SupporterCard;
 
 import org.junit.After;
 import org.junit.Before;
@@ -26,107 +27,107 @@ public class TestGame {
 		HAND_WITH_ALL_COLOURS.add(new ColourCard("", 2, Config.BLUE));
 		HAND_WITH_ALL_COLOURS.add(new ColourCard("", 2, Config.PURPLE));
 	}
-	
-	
+	private static Card SUPPORTER_CARD = new SupporterCard("", 6);
+
 	Game game;
-	
+
 	@Before
 	public void setUp() {
 		game = new Game();
 		game.setNumPlayers(NUM_OF_PLAYERS);
 	}
-	
+
 	@Test
 	public void numPlayersSet() {
 		game.setNumPlayers(NUM_OF_PLAYERS);
-		
+
 		assertEquals(NUM_OF_PLAYERS, game.getNumPlayers());
 	}
-	
+
 	@Test
 	public void addPlayer() {
 		game.addPlayer(PLAYER_ONE_NAME, Config.PURPLE);
-		
+
 		assertNotEquals(null, game.getPlayers()[0]);
 	}
-	
+
 	@Test
 	public void startingPlayer() {
 		game.addPlayer(PLAYER_ONE_NAME, Config.RED);
 		game.addPlayer(PLAYER_TWO_NAME, Config.PURPLE);
-		
+
 		assertEquals(PLAYER_TWO_NAME, game.getCurrentPlayer().getName());
 	}
-	
+
 	@Test
 	public void gameReady() {
 		game.addPlayer(PLAYER_ONE_NAME, Config.RED);
 		game.addPlayer(PLAYER_TWO_NAME, Config.PURPLE);
-		
+
 		assertEquals(true, game.isReadyToStart());
 	}
-	
+
 	@Test
 	public void gameNotReady() {
 		game.addPlayer(PLAYER_ONE_NAME, Config.RED);
-		
+
 		assertEquals(false, game.isReadyToStart());
 	}
-	
+
 	@Test
 	public void gameStart() {
 		game.addPlayer(PLAYER_ONE_NAME, Config.RED);
 		game.addPlayer(PLAYER_TWO_NAME, Config.PURPLE);
-		
+
 		game.startGame();
-		
+
 		assertEquals(false, game.getPlayers()[0].getHandCards().isEmpty());
 		assertEquals(false, game.getTokens().isEmpty());
 	}
-	
+
 	@Test
 	public void playersActiveAfterStart() {
 		game.addPlayer(PLAYER_ONE_NAME, Config.RED);
 		game.addPlayer(PLAYER_TWO_NAME, Config.PURPLE);
-		
+
 		game.startGame();
-		
+
 		assertEquals(true, game.getPlayers()[0].getActive());
 		assertEquals(true, game.getPlayers()[1].getActive());
 	}
-	
+
 	@Test
 	public void startGameTwoPlayersFirstPlayerStarts() {
 		game.addPlayer(PLAYER_ONE_NAME, Config.RED);
 		game.addPlayer(PLAYER_TWO_NAME, Config.PURPLE);
-		
+
 		game.startGame();
-		
+
 		assertEquals(PLAYER_ONE_NAME, game.getCurrentPlayer().getName());
 	}
-	
+
 	@Test
 	public void startGameTwoPlayersSecondPlayerStarts() {
 		game.addPlayer(PLAYER_ONE_NAME, Config.PURPLE);
 		game.addPlayer(PLAYER_TWO_NAME, Config.RED);
-		
+
 		game.startGame();
-		
+
 		assertEquals(PLAYER_TWO_NAME, game.getCurrentPlayer().getName());
 	}
-	
+
 	@Test
 	public void pickTournColourColourInHand() {
 		game.addPlayer(PLAYER_ONE_NAME, Config.RED);
 		game.addPlayer(PLAYER_TWO_NAME, Config.PURPLE);
-		
+
 		game.startGame();
 		game.getCurrentPlayer().clearHand();
-		
-		for (Card c: HAND_WITH_ALL_COLOURS) {
+
+		for (Card c : HAND_WITH_ALL_COLOURS) {
 			game.getCurrentPlayer().addCardToHand(c);
 		}
-		
+
 		game.setTournamentColour(Config.RED);
 		assertEquals(Config.RED, game.getTournamentColour());
 		game.setTournamentColour(Config.YELLOW);
@@ -138,7 +139,21 @@ public class TestGame {
 		game.setTournamentColour(Config.PURPLE);
 		assertEquals(Config.PURPLE, game.getTournamentColour());
 	}
-	
+
+	@Test
+	public void pickTournColourOneSupporterInHand() {
+		game.addPlayer(PLAYER_ONE_NAME, Config.RED);
+		game.addPlayer(PLAYER_TWO_NAME, Config.PURPLE);
+
+		game.startGame();
+		game.getCurrentPlayer().clearHand();
+
+		game.getCurrentPlayer().addCardToHand(SUPPORTER_CARD);
+
+		game.setTournamentColour(Config.BLUE);
+		assertEquals(Config.BLUE, game.getTournamentColour());
+	}
+
 	@After
 	public void tearDown() {
 		game = null;
