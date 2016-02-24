a50a391073bbdeef129dcaacdefa286cf3e67d76
unknown <alisatkaczyk@WIN8-4115-14.windows.scs.carleton.ca>
2016-02-24 12:21:48 -0500

    GUI prepared to merge with client

--- src/main/userinterface/GameReadyWindow.java
+++ src/main/userinterface/GameReadyWindow.java
@@ -1,21 +1,22 @@
 package userinterface;
 
-import java.awt.BorderLayout;
 import java.awt.Dimension;
-import java.awt.GridLayout;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 //needed to use swing components e.g. JFrame public class FirstApplication extends JFrame { public FirstApplication(String title) { super(title); // Set the title of the window
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class GameReadyWindow extends JFrame implements MouseListener{
+public class GameReadyWindow extends JFrame implements ActionListener{
 
 	JLabel token;
+	ImageIcon[] tokens;
 
 	ImageIcon blueToken;
 	ImageIcon greyToken;
@@ -24,13 +25,19 @@ public class GameReadyWindow extends JFrame implements MouseListener{
 	ImageIcon greenToken;
 	ImageIcon goldToken;
 
-	Boolean tokenRequest;
+	JTextField playerName;
+	JButton playerNameSubmit;
+	JButton gameReady;
+	JLabel finalToken;
+	JLabel Step1;
+	JButton tokenRequest;
 
-	ImageIcon[] tokens;
+	JLabel Step2;
+	JLabel Step3 ;
+	JLabel avatar;
 	javax.swing.JFrame frame ;
 
-
-	public GameReadyWindow() {
+	public GameReadyWindow(){
 
 		super(); // Set the title of the window
 		//frame = new javax.swing.JFrame("This appears at the top of the window");
@@ -44,135 +51,159 @@ public class GameReadyWindow extends JFrame implements MouseListener{
 
 		tokens = new ImageIcon[]{blueToken, greyToken, redToken, purpleToken, greenToken, goldToken};
 
-
-
 		setPreferredSize(new Dimension(500,500));
-		setLayout(new GridLayout(2,1)); //entire layout (num cells required, 2 * 1)
 
-		JPanel upperPanel = new JPanel();
-		JPanel lowerPanel = new JPanel();
+		setLayout(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
 
-		upperPanel.setLayout(new GridLayout(1, 2)); //1 column 2 rows, splitting the top row into 2
-		lowerPanel.setLayout(new GridLayout(1,1));
+		 Step1 = new JLabel("Step 1: Enter your Name");
+		c.fill = GridBagConstraints.HORIZONTAL;
 
-		JPanel playerSide = new JPanel(new BorderLayout());
-		JPanel tokenSide = new JPanel();
-		JPanel readySide = new JPanel();
+		c.ipady = 10;      //make this component tall
+		c.weightx = 0.5;
+		c.gridx = 0;
+		c.gridy = 0;
+		add(Step1, c);
 
-		JLabel Step1 = new JLabel("Step 1: Enter your Name");
-		JLabel Step2 = new JLabel("Step 2: Click here to pick a token");
-		JLabel Step3 = new JLabel("Step 3: PREPARE TO BATTLE");
-
-
-		JLabel avatar = new JLabel();
+		 avatar = new JLabel();
 		avatar.setIcon(new ImageIcon(this.getClass().getResource("Images/Avatar.jpg")));
+		c.ipady = avatar.getHeight();     
+		c.weightx = 0.5;
+		c.gridx = 0;
+		c.gridy = 1;
+		add(avatar, c);
 
-		JTextField playerName = new JTextField();	
+
+		playerName = new JTextField();	
 		playerName.setPreferredSize(new Dimension(100, 50));	
+		c.ipady = 10;   
+		c.weightx = 0.5;
+		c.gridx = 0;
+		c.gridy = 3;
+		add(playerName, c);
 
-		JLabel gameReady = new JLabel();		
-		gameReady.setIcon(new ImageIcon(this.getClass().getResource("Images/GameReady.jpg")));
-		gameReady.setName("game ready");
-		gameReady.addMouseListener(this);
+		playerNameSubmit = new JButton("Submit");
+		c.ipady = 10;    
+		c.weightx = 0.5;
+		c.gridx = 0;
+		c.gridy = 4;
+		add(playerNameSubmit, c);
+		playerNameSubmit.addActionListener(this);
+
+
+		Step2 = new JLabel("Step 2: Click here to pick a token");
+		c.fill = GridBagConstraints.CENTER;
+		c.ipady = 10;   
+		c.weightx = 0.5;
+		c.gridx = 2;
+		c.gridy = 0;
+
+		Step2.setVisible(false);
+		add(Step2, c);
 
 		token = new JLabel();
-		token.setIcon(tokens[1]);
+		token.setIcon(new ImageIcon(this.getClass().getResource("Images/Avatar.jpg")));
+		c.ipady = token.getHeight();   
+		c.weightx = 0.5;
+		c.gridx = 2;
+		c.gridy = 1;
 
-		JLabel tokenSelect = new JLabel("Press to Retrieve a Token");
-		//tokenSelect.setIcon(new ImageIcon(this.getClass().getResource("Images/Token.jpg")));
-		tokenSelect.setName("token select");
-		tokenSelect.addMouseListener(this);
+		token.setVisible(false);
+		add(token, c);
 
-		JLabel finalToken = new JLabel("This is the token you retrieved");
+		tokenRequest = new JButton("Press to Retrieve a Token");
+		c.weightx = 0.8;
+		c.gridx = 2;
+		c.gridy = 3;
+
+		tokenRequest.setVisible(false);
+		add(tokenRequest, c);
+		
+		tokenRequest.addActionListener(this);
+
+
+		Step3 = new JLabel("Step 3: PREPARE TO BATTLE");
+		c.fill = GridBagConstraints.CENTER;
+		c.ipady = 10;   
+		c.weightx = 0.5;
+		c.gridx = 3;
+		c.gridy = 0;
+		Step3.setVisible(false);
+		add(Step3, c);
+
+		//THE FINAL TOKEN IS THE VALUE RECIEVED FROM THE SERVER AND UPDATES THE ICON ACCORDINGLY. MUST BE DONE SEPARATELY.
+		 finalToken = new JLabel();//"This is the token you retrieved");
 		finalToken.setIcon(new ImageIcon(this.getClass().getResource("Images/Token.jpg")));
+		c.ipady = finalToken.getHeight();   
+		c.weightx = 0.5;
+		c.gridx = 3;
+		c.gridy = 1;
 
+		finalToken.setVisible(false);
+		add(finalToken, c);
 
-		Step1.setHorizontalAlignment(JLabel.CENTER);
-		Step1.setVerticalAlignment(JLabel.BOTTOM);
-		avatar.setHorizontalAlignment(JLabel.CENTER);
-		playerName.setHorizontalAlignment(JLabel.CENTER);
-		playerSide.add(Step1, BorderLayout.NORTH);
-		playerSide.add(avatar, BorderLayout.CENTER);
-		playerSide.add(playerName, BorderLayout.SOUTH);
+		 gameReady = new JButton("Game Ready");
+		//gameReady.setIcon(new ImageIcon(this.getClass().getResource("Images/GameReady.jpg")));
+		c.weightx = 0.8;
+		c.gridx = 3;
+		c.gridy = 3;
 
-
-		Step2.setHorizontalAlignment(JLabel.CENTER);
-		Step2.setVerticalAlignment(JLabel.BOTTOM);
-		token.setHorizontalAlignment(JLabel.CENTER);
-		tokenSelect.setHorizontalAlignment(JLabel.CENTER);
-
-		tokenSide.add(Step2, BorderLayout.NORTH);
-		tokenSide.add(token, BorderLayout.CENTER);
-		tokenSide.add(tokenSelect, BorderLayout.SOUTH);
-
-		Step3.setHorizontalAlignment(JLabel.CENTER);
-		Step3.setVerticalAlignment(JLabel.BOTTOM);
-		gameReady.setHorizontalAlignment(JLabel.CENTER);
-		readySide.add(Step3, BorderLayout.NORTH);
-		readySide.add(gameReady, BorderLayout.CENTER);
-		readySide.add(finalToken, BorderLayout.SOUTH);
-
-		upperPanel.add(playerSide);
-		upperPanel.add(tokenSide);
-		lowerPanel.add(readySide);
-
-		add(upperPanel);
-		add(lowerPanel);
+		gameReady.setVisible(false);
+		add(gameReady, c);
+		gameReady.addActionListener(this);
 
 		setVisible(true);
 		pack();
 		setDefaultCloseOperation(EXIT_ON_CLOSE); // allow window to close
-		setSize(300, 100); // Set the size of the window
+		setSize(500, 500); // Set the size of the window
 
 	}
 
-	public static void main(String args[]) { // Instantiate a FirstApplication object so you can display it FirstApplication frame = new FirstApplication("FirstApplication
-		GameReadyWindow startWindow = new GameReadyWindow();
-		startWindow.setVisible(true);
 
-	}
 
-	@Override
-	public void mouseClicked(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		//when mouse clicked on retrieve token
-		//tokenRequest = true;
-	}
+//	public static void main(String args[]) { // Instantiate a FirstApplication object so you can display it FirstApplication frame = new FirstApplication("FirstApplication
+//		GameReadyWindow startWindow = new GameReadyWindow("");
+//		startWindow.setVisible(true);
+//	}
 
 
 	@Override
-	public void mouseEntered(MouseEvent arg0) {
-		// TODO Auto-generated method stub
+	public void actionPerformed(ActionEvent e) {
+		String action = e.getActionCommand();
+		if (action.equals("Submit")) {
+
+			System.out.println(playerName.getText());
+			Step2.setVisible(true);
+
+			token.setVisible(true);
+			tokenRequest.setVisible(true);
+			
+			Step3.setText("Step 3: " + playerName.getText() + " PREPARE TO BATTLE");
+			Step2.setText("Step 2: " + playerName.getText() + " Press to Retrieve a Token");
+			//SEND THE PLAYERS NAME TO BE STORED.
+			playerName.setText(" ");
+
+		}
+		else if (action.equals("Press to Retrieve a Token")){
+			System.out.println("tokenRequest");
+			Step3.setVisible(true);
+			finalToken.setVisible(true);
+			gameReady.setVisible(true);
+			//SEND THE REQUEST FOR A TOKEN GUI CONTROL WHO WILL SEND TO SERVER?
+			//OR JUST SEND TO THE SERVER
+			//client.send(
+		}
+		else if(action.equals("Game Ready")){
+			System.out.println("Game Ready");
+			//SEND MESSAGE GAME READY TO GUI CONTROL WHO WILL SEND TO SERVER?
+			//OR JUST SEND TO THE SERVER
+		}
 
 	}
-
-
-	@Override
-	public void mouseExited(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-
-	}
-
-
-	@Override
-	public void mousePressed(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-
-	}
-
-
-	@Override
-	public void mouseReleased(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-
-	}
-
-	public boolean getTokenRequest(){
-		return tokenRequest;
+	
+	public void getFinalToken(){
+		//RECIEVE RANDOM TOKEN FROM THE SERVER, UPDATE THE SCREEN
+		
 	}
 }
-
-
-
-
 
