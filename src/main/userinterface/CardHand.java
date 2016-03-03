e8fdcc512c13880bb767087164490b0f97fa52c3
unknown <alisatkaczyk@WIN8-4115-28.windows.scs.carleton.ca>
2016-03-03 17:51:52 -0500

    Can send multiple cards.

--- src/main/userinterface/CardHand.java
+++ src/main/userinterface/CardHand.java
@@ -110,6 +110,7 @@ public class CardHand extends JPanel implements ActionListener {
 		}
 		else if (action.equals("Play Card")) {
 			System.out.println("YOU JUST PLAYED " + cardToSend);
+			cardToSend = "";
 			maxCards = false;
 			gui.sendCardToPlay(cardToSend); 
 		}
