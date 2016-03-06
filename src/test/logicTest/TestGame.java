2303eca52f26d3a16875b5e42e7651e24e20107e
Sophia <sophia.c.b@hotmail.com>
2016-03-06 12:40:34 -0500

    Counter charge not allowed to play test pass

--- src/test/logicTest/TestGame.java
+++ src/test/logicTest/TestGame.java
@@ -819,6 +819,25 @@ public class TestGame {
 		assertEquals(true,
 				game.getPlayer(0).getDisplayCards().contains(PURPLE_CARD_7));
 	}
+	
+	@Test
+	public void notAllowedToPlayCountercharge() {
+		game.setNumPlayers(3);
+		game.addPlayer(PLAYER_ONE_NAME, Config.RED);
+		game.addPlayer(PLAYER_TWO_NAME, Config.PURPLE);
+		game.addPlayer(PLAYER_THREE_NAME, Config.PURPLE);
+
+		game.getPlayer(0).addCardToDisplay(BLUE_CARD_3, Config.BLUE);
+
+		game.getPlayer(1).addCardToHand(COUNTER_CHARGE_CARD);
+		String result = game.playCard(1, COUNTER_CHARGE_CARD.getName());
+		
+		assertEquals(true, result.contains("false"));
+		assertEquals(1, game.getPlayer(1).getHandCards().size());
+		assertEquals(0, game.getDiscardPileSize());
+		assertEquals(1, game.getPlayer(0).getDisplayCards().size());
+		assertEquals(0, game.getPlayer(2).getDisplayCards().size());
+	}
 
 	@After
 	public void tearDown() {
