973bc8bc37d3e79e709a5e7e7edfb7d6d4bd98d2
unknown <alisatkaczyk@WIN8-4115-07.windows.scs.carleton.ca>
2016-03-06 14:45:29 -0500

    Brand new game ready window

--- src/main/userinterface/GamePlayButtonPanel.java
+++ src/main/userinterface/GamePlayButtonPanel.java
@@ -57,6 +57,7 @@ public class GamePlayButtonPanel extends JPanel implements ActionListener{
 		String action = e.getActionCommand();
 		if (action.equals("Withdraw")) {
 			gui.requestToWithdraw();
+			gui.setEnableMainScreen("false");
 		}
 		else if (action.equals("Draw a Card")) {
 			gui.requestToDrawCard();
