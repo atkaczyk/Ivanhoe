482d17ad45dafd88dd389e72e83e5577e854e6f7
Sophia <sophia.c.b@hotmail.com>
2016-03-06 17:15:34 -0500

    Comments in all card classes

--- src/main/logic/SimpleCard.java
+++ src/main/logic/SimpleCard.java
@@ -1,11 +1,17 @@
 package logic;
 
+/**
+ * Represents all the simple cards in the Ivanhoe card game, see {@link Card}
+ * 
+ * @author Sophia
+ * 
+ */
 public abstract class SimpleCard extends Card {
-	
+	// The number value of the card
 	protected int number;
 
 	public int getNumber() {
 		return number;
 	}
-	
+
 }
