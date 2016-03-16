03cfe6a0c2ffedca91312fbaa60aa2e4b038debb
Sophia <sophia.c.b@hotmail.com>
2016-03-15 22:20:04 -0400

    Not allowed to play retreat card test pass

--- src/test/logicTest/TestGame.java
+++ src/test/logicTest/TestGame.java
@@ -1347,8 +1347,27 @@ public class TestGame {
 		game.getPlayer(0).addCardToHand(RETREAT_CARD);
 
 		String result = game.playCard(0, RETREAT_CARD.getName());
-		System.out.println(result);
 		assertEquals(true, result.contains("moreInformationNeeded"));
+		assertEquals(0, game.getDiscardPileSize());
+		assertEquals(1, game.getPlayer(0).getHandCards().size());
+	}
+	
+	@Test
+	public void notAllowedToPlayRetreat() {
+		game.setNumPlayers(2);
+		game.addPlayer(PLAYER_ONE_NAME, Config.RED);
+		game.addPlayer(PLAYER_TWO_NAME, Config.PURPLE);
+
+		game.getPlayer(1).addCardToDisplay(PURPLE_CARD_3, Config.PURPLE);
+		game.getPlayer(1).addCardToDisplay(SQUIRE_CARD_2, Config.PURPLE);
+		
+		game.getPlayer(0).addCardToDisplay(PURPLE_CARD_3, Config.PURPLE);
+		
+		game.getPlayer(0).addCardToHand(RETREAT_CARD);
+
+		String result = game.playCard(0, RETREAT_CARD.getName());
+		System.out.println(result);
+		assertEquals(true, result.contains("false"));
 		assertEquals(0, game.getDiscardPileSize());
 		assertEquals(1, game.getPlayer(0).getHandCards().size());
 	}
