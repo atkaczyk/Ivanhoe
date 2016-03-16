069c43bab380af54b5a875202dbe20fe7d3f178b
Sophia <sophia.c.b@hotmail.com>
2016-03-15 21:53:47 -0400

    Play dodge more info needed test fail

--- src/test/logicTest/TestGame.java
+++ src/test/logicTest/TestGame.java
@@ -56,6 +56,7 @@ public class TestGame {
 	private static final Card UNHORSE_CARD = new ActionCard("Unhorse");
 	private static final Card CHANGE_WEAPON_CARD = new ActionCard("Change Weapon");
 	private static final Card BREAK_LANCE_CARD = new ActionCard("Break Lance");
+	private static final Card DODGE_CARD = new ActionCard("Dodge");
 
 	Game game;
 
@@ -1256,6 +1257,29 @@ public class TestGame {
 		assertEquals(1, game.getPlayer(2).getDisplayCards().size());
 		assertEquals(true, game.getPlayer(2).getDisplayCards().contains(PURPLE_CARD_7));
 		assertEquals(false, game.getPlayer(2).getDisplayCards().contains(PURPLE_CARD_3));
+	}
+	
+	@Test
+	public void tryPlayingDodgeMoreInfoNeeded() {
+		game.setNumPlayers(3);
+		game.addPlayer(PLAYER_ONE_NAME, Config.RED);
+		game.addPlayer(PLAYER_TWO_NAME, Config.PURPLE);
+		game.addPlayer(PLAYER_THREE_NAME, Config.BLUE);
+
+		game.getPlayer(1).addCardToDisplay(PURPLE_CARD_3, Config.PURPLE);
+		
+		game.getPlayer(2).addCardToDisplay(SQUIRE_CARD_2, Config.PURPLE);
+		
+		game.getPlayer(0).addCardToDisplay(PURPLE_CARD_3, Config.PURPLE);
+		game.getPlayer(0).addCardToDisplay(PURPLE_CARD_3, Config.PURPLE);
+		
+		game.getPlayer(0).addCardToHand(DODGE_CARD);
+
+		String result = game.playCard(0, DODGE_CARD.getName());
+		System.out.println(result);
+		assertEquals(true, result.contains("moreInformationNeeded"));
+		assertEquals(0, game.getDiscardPileSize());
+		assertEquals(1, game.getPlayer(0).getHandCards().size());
 	}
 
 	@After
