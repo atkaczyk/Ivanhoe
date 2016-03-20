d3368dd5cf363b6b9d268e7ee006f93781ac3e14
Victoria Gray <Victoria Gray>
2016-03-20 16:37:46 -0400

    Start server and client now enters ip and port. So it should work on multiple computers

--- src/main/network/StartClient.java
+++ src/main/network/StartClient.java
@@ -1,12 +1,35 @@
 package network;
 
+import javax.swing.JOptionPane;
+
 import network.Client;
 import utils.Config;
 
 public class StartClient {
 
 	public static void main (String[] argv){
-		new Client(Config.DEFAULT_HOST, Config.DEFAULT_PORT);
+		
+		//Asking user for host
+		String host = (String)JOptionPane.showInputDialog(
+				null,
+		        "Enter the Server Host IP Adress (eg. 127.0.0.1)",
+		        "Customized Dialog",
+		        JOptionPane.PLAIN_MESSAGE,
+		        null,
+		        null,
+		        null);
+		
+		//Asking user for port
+		String port = (String)JOptionPane.showInputDialog(
+				null,
+		        "Enter the Client Port (eg. 5050)",
+		        "Customized Dialog",
+		        JOptionPane.PLAIN_MESSAGE,
+		        null,
+		        null,
+		        null);
+		
+		new Client(host, Integer.parseInt(port));		//dont use defaults. make popup to ask client for info
 	}
 	
 }
