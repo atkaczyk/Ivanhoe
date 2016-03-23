7bbc84249de272a7692ea3acfc4617bc134bf2d9
Sophia <sophia.c.b@hotmail.com>
2016-03-23 13:13:46 -0400

    Play adapt card need more info test fail

--- src/test/logicTest/TestGame.java
+++ src/test/logicTest/TestGame.java
@@ -1901,6 +1901,32 @@ public class TestGame {
 		assertEquals(1, game.getPlayer(0).getHandCards().size());
 		assertEquals(2, game.getDiscardPileSize());
 	}
+	
+	@Test
+	public void playAdaptCardAskForIvanhoe() {
+		game.setNumPlayers(2);
+		game.addPlayer(PLAYER_ONE_NAME, Config.RED);
+		game.addPlayer(PLAYER_TWO_NAME, Config.PURPLE);
+
+		game.getPlayer(0).addCardToDisplay(SQUIRE_CARD_3, Config.PURPLE);
+		game.getPlayer(0).addCardToDisplay(SQUIRE_CARD_3, Config.PURPLE);
+		game.getPlayer(0).addCardToDisplay(SQUIRE_CARD_2, Config.PURPLE);
+		game.getPlayer(0).addCardToHand(ADAPT_CARD);
+		
+		game.getPlayer(1).addCardToDisplay(SQUIRE_CARD_3, Config.PURPLE);
+		game.getPlayer(1).addCardToDisplay(SQUIRE_CARD_3, Config.PURPLE);
+		game.getPlayer(1).addCardToDisplay(SQUIRE_CARD_2, Config.PURPLE);
+		game.getPlayer(1).addCardToHand(IVANHOE_CARD);
+
+		String result = game.playCard(0, ADAPT_CARD.getName());
+		System.out.println(result);
+		assertEquals(true, result.contains("Ivanhoe"));
+		assertEquals(1, game.getPlayer(0).getHandCards().size());
+		assertEquals(1, game.getPlayer(1).getHandCards().size());
+		assertEquals(3, game.getPlayer(0).getDisplayCards().size());
+		assertEquals(3, game.getPlayer(1).getDisplayCards().size());
+		assertEquals(0, game.getDiscardPileSize());
+	}
 
 	@After
 	public void tearDown() {
